291a8e7aa3139c23bb0f7530cde9d68b0d568c81
Brian C Sparks <brian.c.sparks@hp.com>
2019-01-02 13:06:17 -0800

    WIP

--- packages/sg-flow/index.js
+++ packages/sg-flow/index.js
@@ -4,8 +4,7 @@
  */
 
 var _                         = require('lodash');
-
-
+var sg                        = require('sg0');
 
 /**
  *  Was the callback called to mean a good result? (Are the results OK?)
@@ -20,7 +19,7 @@ var _                         = require('lodash');
  *
  *          if (!ok(err, result1, result2)) { return err; }
  */
-sg.ok = function(err /*, [argN]*/) {
+sg.okv = function(err /*, [argN]*/) { /* verbose */
   if (err)  { console.error(err); return false; }
 
   var result = true;
@@ -49,7 +48,7 @@ sg.ok = function(err /*, [argN]*/) {
  *
  *          if (!ok(err, result1, result2)) { return err; }
  */
-sg.okQuiet = sg.okq = function(err /*, [argN]*/) {
+sg.ok = function(err /*, [argN]*/) {
   if (err)  { return false; }
 
   var result = true;
@@ -159,5 +158,474 @@ sg.skip = function(msg_, next) {
   return next();
 };
 
+/**
+ * The internal workhorse for all the flows that run sequentailly.
+ *
+ * @param {*} coll
+ * @param {*} fn
+ * @param {*} callback
+ * @returns
+ */
+var __each_ = function(coll, fn, callback) {
+
+  if (_.isArray(coll) && coll.length <= 0) {
+    return callback();
+  }
+
+  var i = 0, end;
+  var indexes, values, errors, hasError = false;
+
+  if (_.isArray(coll)) {
+    indexes = _.range(coll.length);
+    values = [];
+    errors = [];
+  }
+  else {
+    indexes = _.keys(coll);
+    values = {};
+    errors = {};
+  }
+
+  if ((end = indexes.length) === 0) {
+    return callback(hasError ? errors : null, values);
+  }
+
+  var doOne = function() {
+    var item = coll[indexes[i]];
+    var nextI = i + 1;
+    var next = function(err, val) {
+      if (err) { hasError = true; }
+
+      errors[indexes[i]] = err;
+      values[indexes[i]] = val;
+
+      i = nextI;
+      if (i < end) {
+        return process.nextTick(function() {
+          doOne();
+        });
+      }
+
+      return callback(hasError ? errors : null, values);
+    };
+
+    return fn(item, next, indexes[i], coll);
+  };
+
+  return setImmediate(doOne);
+};
+
+/**
+ * Iterate over each item in a collection and call a 'callbackified' function on each one.
+ *
+ * The `callback` parameter can be first or last, and is determined by type.
+ *
+ * * Passing it last conforms to the Node.JS calling convention.
+ * * Passing it first is very convienent, if you have a named function, like
+ *   `next` or `callback`.
+ *
+ * @param {*} coll
+ * @param {*} fn
+ * @param {*} callback
+ * @returns
+ */
+sg.__each = function(a, b, c) {
+  // If the caller has a named function, like 'next', it is easier to call this
+  // function with that name first
+  if (_.isFunction(a)) { return __each_(b, c, a); }
+
+  // Normal
+  return __each_(a, b, c);
+};
+
+/**
+ * Run each function sequentially.
+ *
+ * The arguments can be in either order, and are determined by type.
+ *
+ * @param {*} fns
+ * @param {*} onDone
+ * @returns
+ */
+sg.__run = function(a, b) {
+  var fns, callback;
+
+  if (_.isArray(a)) {
+    fns = a; callback = b;
+  } else {
+    fns = b; callback = a;
+  }
+
+  return sg.__each(
+    fns,
+    function(fn, next, index, coll) {
+      return fn(next, index, coll);
+    },
+    callback || function() {}
+  );
+};
+
+/**
+ *  Runs a list of Node.js-style functions synchronously.
+ *
+ *  This is a version of __run() that has a couple of extra features for typical handlers.
+ *
+ *  * Passes a `self` parameter that can be used to share data/state between the functions.
+ *    Works great to accumulate the function's result.
+ *  * Provides the finalization function to each function, so it can be called at any point.
+ *  * Provides two finalization functions, intending one for normal/success results, and one
+ *    for errors.
+ *  * Very flexible parameter signature. Detects each param by its type. The only constraints
+ *    are that `self` must be first, if provided; and that `abort` comes after `last`.
+ *
+ *  This function was built for two specific use-cases that occur a lot. It allows to provide
+ *  a `main` function first in the list, to improve readability; and allows `callback` to be
+ *  passed in (usually before the functions), and then calling `last` or `abort` in any function
+ *  is really calling `callback`.
+ *
+ *  If the last function in the Array calls `next()` (which is very typical), then last will
+ *  be called as `last(null, self)`. This is obviously to facilitate using `callback` as `last`.
+ *  If you do not want `self` to be passed automatically like this, pass your object in double-
+ *  wrapped in Arrays: `[[{}]]`. The wrapping Arrays are just decoration, and will be removed
+ *  before anything is done with `self`.
+ *
+ *  @param {Object} self    - An object passed to each function. Makes it easy for the functions
+ *                            to share state/data. Must be the first parameter. If it is not supplied
+ *                            __run2() will provide `{}`, and pass as the _last_ parameter to each
+ *                            of the functions. If provided, it is passed as the first parameter.
+ *  @param {function[]} fns - An Array of functions to be run on after the other.
+ *  @param {function} last  - The last function to be run. This is a separate function outside the
+ *                            Array of functions. It may be provided in any location in the parameter
+ *                            list.
+ *  @param {function} abort - A separate function to be used as a final function. If not provided, will
+ *                            be a copy of `last`. To skip providing a function, but to have `abort` not
+ *                            be a copy of `last`, pass `false`. `abort` will always put a truthy
+ *                            value in the first parameter (using `'aborted'` as a default.) This
+ *                            allows you to just `return abort();` in any function.
+ */
+sg.__run2 = function(a,b,c,d) {   // self, fns, last, abort
+  // Figure out params
+  var args = _.toArray(arguments);
+  var self,fns,last,abort_,abort,privateSelf,noSelf;
+
+  // self can only be the first param, if not, use a blank Object
+  self          = isObject(args[0]) && args.shift();
+  privateSelf   = !self;                                  // If self is undefined here, it is potentially a private-self
+
+  self          = self || _.isArray(args[0]) && args[0].length === 1 && _.isArray(args[0][0]) && args[0][0].length === 1 && args[0][0][0];
+  privateSelf   = privateSelf && !!self;                  // But if self wasn't set by [[...]], it is not private-self
+
+  noSelf        = !self;
+  self          = self || {};
+
+  // Fns is the only Array
+  args = _.filter(args, function(arg) { return fns || !(fns = _.isArray(arg) && arg); });
+
+  // last is the first function
+  args = _.filter(args, function(arg) { return last || !(last = _.isFunction(arg) && arg); });
+
+  // any remaining arg has to be abort
+  abort_ = args.shift();
+
+  // Any of them can be unset
+  fns   = fns   || [];
+  last  = last  || function(){};
+
+  if (abort_ === false)  { abort_ = function(){}; }
+  else                   { abort_ = abort_ || last; }
+
+  abort = function(err) {
+    var args = _.rest(arguments);
+    args.unshift(err || 'aborted');
+
+    // Make sure it is a function, then call it
+    if (_.isFunction(abort_)) {
+      return abort_.apply(this, args);
+    }
+  };
+
+  return sg.__each(
+    fns,
+    function(fn, next, index, coll) {
+      if (noSelf) {
+        return fn(next, last, abort, self);
+      }
+      return fn(self, next, last, abort);
+    },
+    function(a,b) {
+      if (privateSelf) {
+        return last.apply(this, arguments);
+      }
+
+      return last(null, self);
+    }
+  );
+};
+
+/**
+ *  Replaces all attributes on result with those on all the rest of the arguments.
+ *
+ *  When using __run2, the result of the operation is passed into each step, so you can
+ *  add attributes to the result. However, using this style you cannot set the whole
+ *  result object at once. (You cannot do `result = {ack:'bar'}`) You would have to remove
+ *  all existing attributes, and add all new ones -- thats what this function does.
+ *
+ */
+sg.replaceResult = function(result /*, ...replaces*/) {
+
+  var replaces = _.rest(arguments);
+
+  // First, clobber the result
+  _.each(result, function(value, key) {
+    delete result[key];
+  });
+
+  // Then, loop over all the other fn params, and add those to result
+  _.each(replaces, function(replace) {
+    _.each(replace, function(value, key) {
+      result[key] = value;
+    });
+  });
+}
 
 
+/**
+ *  Calls fn until it wants to quit
+ */
+sg.until = function(/* [options,] fn, callback */) {
+  var args      = _.toArray(arguments);
+  var callback  = args.pop();
+  var fn        = args.pop();
+  var options   = args.shift() || {};
+
+  var max       = options.max;
+  var timeout   = options.timeout;
+
+  options.interval  = options.interval || options.delay;
+
+  var count = -1, start = _.now();
+
+  var again;
+  var once = function() {
+    count += 1;
+
+    // Limit the number of executions
+    if (options.max) {
+      if (count >= max)                 { return callback(toError("Too many executions in until(): "+count)); }
+    }
+
+    // Limit the time it can run
+    if (options.timeout) {
+      if (timeout > (_.now() - start))  { return callback(toError("Timeout in until: ("+(_.now() - start)+" ms.)")); }
+    }
+
+    return fn(again, callback, count, _.now() - start);
+  };
+
+  again = function() {
+    var delay = options.interval;
+
+    if (arguments.length > 0) {
+      delay = arguments[0];
+    }
+
+    if (delay) {
+      return setTimeout(once, delay);
+    }
+
+    /* otherwise */
+    return once();
+  };
+
+  // Yes, this actually works
+  again.uncount = function(num_) {
+    count -= (num_ || 1);
+  };
+
+  // Yes, this actually works
+  again.uncountSometimes = function(num_) {
+    if (Math.random() > 0.25) {
+      return again.uncount.apply(this, arguments);
+    }
+  };
+
+  return once();
+};
+
+
+
+
+
+
+/**
+ * Send each item in the collection to the fn, in parallel.
+ *
+ * @param {*} coll
+ * @param {*} max
+ * @param {*} fn
+ * @param {*} onDone
+ * @returns
+ */
+var __eachll = sg.__eachll = function(list_ /*, max_, fn_, callback_*/ ) {
+
+  var args      = _.rest(arguments);
+  var callback  = args.pop();
+  var fn        = args.pop();
+  var max       = args.length > 0 ? args.shift() : 10000000;
+
+  if (_.isArray(list_)) {
+    var list = list_.slice();
+
+    if (list.length === 0) { return callback(); }
+
+    var outstanding = 0;
+    var launch = function(incr) {
+      outstanding += (incr || 0);
+
+      if (list.length > 0 && outstanding < max) {
+        outstanding++;
+        fn(list.shift(), function() {
+          process.nextTick(function() {
+            launch(-1);
+          });
+        }, list_.length - list.length - 1, list_);
+        //process.nextTick(launch);
+        launch();
+      }
+      else if (list.length === 0 && outstanding === 0) {
+        callback();
+      }
+    };
+    launch(1);
+    outstanding -= 1;
+    return;
+  }
+
+  /* otherwise */
+  return sg.__eachll(_.keys(list_), max, function(key, nextKey) {
+    fn(list_[key], nextKey, key, list_);
+  }, callback);
+};
+
+/**
+ * Run the `fns` in parallel, running `max` at one time.
+ *
+ * @param {*} fns
+ * @param {*} max
+ * @param {*} onDone
+ * @returns
+ */
+sg.__runll = function(/*fns, max, onDone*/) {
+
+  var args    = _.rest(arguments, 0);
+  var onDone  = args.pop();
+
+  // The dispatch function
+  args.push(function(fn /*, next, index, coll*/) {
+    // fn(next, index, coll)
+    return fn.apply(this, _.rest(arguments));
+  });
+
+  // The final function
+  args.push(onDone);
+
+  return __eachll.apply(this, args);
+};
+
+/**
+ *  Internally wrap a function so error handling is not so boiler-plateish.
+ */
+sg.iwrap = function(myname, fncallback /*, abort, body_callback*/) {
+  var   args             = _.rest(arguments, 2);
+  const body_callback    = args.pop();
+  var   abort            = args.shift();
+
+  abort = abort || function(err, msg) {
+    if (msg)  {
+      console.error(myname, msg);
+    }
+    return fncallback(err);
+  };
+
+  var abortCalling;
+  var abortParams;
+
+  var eabort = function(callback, abortCalling2) {
+    return function(err) {
+      if (!err) { return callback.apply(this, arguments); }
+
+      const abortCalling_ = abortCalling || abortCalling2;     abortCalling  = null;
+      const abortParams_  = abortParams;                       abortParams   = null;
+
+      var msg = '';
+
+      if (abortCalling_) {
+        msg += myname + '__' + abortCalling_;
+      }
+
+      if (abortParams_) {
+        msg += ': ' + abortParams;
+      }
+
+      return abort(err, msg);
+    };
+  };
+
+  eabort.p = function(params_) {
+    const params = _.isObject(params_) ? params_ : { param: params_};
+    abortParams = JSON.stringify(params);
+    return params_;
+  };
+
+  eabort.calling = function(calledName, params) {
+    abortCalling  = null;
+    abortParams   = null;
+
+    if (calledName) {
+      abortCalling = calledName;
+    }
+
+    if (params) {
+      eabort.p(params);
+    }
+  };
+
+  return body_callback(eabort);
+};
+
+var example = function(a, b, callback) {
+  return sg.iwrap('example', callback, function(abort) {
+
+    return sg.__run2({}, callback, [function(result, next, last) {
+      var   params = {x:42};
+
+      // If we end up calling `abort`, this info is available
+      abort.calling('s3put');
+      abort.p(params);
+
+      // Call a standard Node.JS style fn
+      return s3put(params, function(err, data) {
+        if (!sg.ok(err, data)) { return abort(err); }
+
+        result.x = data.y;
+
+        // Can exit early
+        if (data.completeSuccess) {
+          return last(null, data);
+        }
+
+        return next();
+      });
+    }, function(result, next) {
+      result.ok = true;
+
+      // Calling `next()` will send `result` to `callback(null, result)`
+      return next();
+    }]);
+  });
+};
+
+// Export functions
+_.each(sg, function(fn, name) {
+  exports[name] = sg[name];
+});
--- packages/sg0/index.js
+++ packages/sg0/index.js
@@ -8,6 +8,16 @@ var util                      = require('util');
 
 var   sg = {};
 
+var seconds = sg.seconds = sg.second = 1000,        second = seconds;
+var minutes = sg.minutes = sg.minute = 60*seconds,  minute = minutes;
+var hours   = sg.hours   = sg.hour   = 60*minutes,  hour   = hours;
+var days    = sg.days    = sg.day    = 24*hours,    day    = days;
+var weeks   = sg.weeks   = sg.week   = 7*days,      week   = weeks;
+var months  = sg.months  = sg.month  = 30*days,     month  = months;
+var years   = sg.years   = sg.year   = 365*days,    year   = years;
+
+
+
 /**
  * Returns an inspected object.
  *
@@ -17,6 +27,493 @@ var   sg = {};
  */
 sg.inspect = function(x, colors) {
   return util.inspect(x, {depth:null, colors: colors || false});
+};
+
+/**
+ *  Just like setTimeout, but with the parameters in the right order.
+ */
+sg.setTimeout = function(ms, cb) {
+  return setTimeout(cb, ms);
+};
+
+sg.firstKey = function(obj) {
+  for (var k in obj) {
+    return k;
+  }
+  return ;
+};
+
+sg.numKeys = function(obj) {
+  var num = 0;
+  for (var k in obj) {
+    num++;
+  }
+
+  return num;
+};
+
+/**
+ *  Build {k:v}
+ */
+var kv = sg.kv = function(o, k, v) {
+  if (arguments.length === 2) {
+    return kv(null, o, k);
+  }
+
+  o = o || {};
+
+  if (isnt(k))              { return o; }
+  if (_.isUndefined(v))     { return o; }
+
+  o[k] = v;
+  return o;
+};
+
+/**
+ *  Build {key:k, vName:v}
+ */
+var kkvv = sg.kkvv = function(o, k, v, vName) {
+  if (arguments.length === 2) {
+    return kkvv(null, o, k, 'value');
+  }
+
+  if (arguments.length === 3) {
+    return kkvv(null, o, k, v);
+  }
+
+  o         = o || {};
+  o.key     = k;
+  o[vName]  = v;
+
+  return o;
+};
+
+/**
+ *  Build {k:v}, where the key is a dotted-key
+ */
+var dottedKv = sg.dottedKv = function(o, k, v) {
+  if (arguments.length === 2) {
+
+    if (_.isArray(o)) { return dottedKv(null, o.join('.'), k); }
+    return kv(null, o, k);
+  }
+
+  if (_.isArray(k)) { return dottedKv(o, k.join('.'), v); }
+
+  o = o || {};
+  o[k] = v;
+  return o;
+};
+
+/**
+ *  Build [v]
+ *
+ *  Just like kv(), so you can return ap(m, 42) or ap(42)
+ */
+var ap = sg.ap = function(a, v) {
+  if (arguments.length === 1)   { return sg.ap(null, arguments[0]); }
+
+  a = a || [];
+
+  if (_.isUndefined(v))         { return a; }
+
+  a.push(v);
+  return a;
+};
+
+/**
+ *  Pushes the item into the array, and returns the index of where it got
+ *  pushed at.
+ *
+ *  If x is undefined, `_push()` will not push it, and returns undefined.
+ */
+sg._push = function(arr, x) {
+  if (_.isUndefined(x))     { return x; }
+
+  const length = arr.length;
+
+  arr.push(x);
+
+  return length;
+};
+
+/**
+ *  Returns the keys of an object.
+ *
+ *  Just like _.keys, except it will return null or undefined if given an
+ *  input that isnt().
+ */
+sg.keys = function(x) {
+  if (isnt(x))            { return x; }
+
+  return _.keys(x);
+};
+
+/**
+ *  Makes an object where the key for each item is the same as the value.
+ */
+sg.keyMirror = function(x, sep) {
+  var result = {};
+
+  if (isnt(x))            { return x; }
+
+  if (_.isString(x))      { return sg.keyMirror(x.split(sep || ',')); }
+  if (sg.isObject(x))     { return sg.keyMirror(_.keys(x)); }
+
+  if (!_.isArray(x))      { return result; }
+
+  _.each(x, function(item) {
+    result[item] = item;
+  });
+
+  return result;
+};
+
+/**
+ *  Is the parameter strictly an Object (and not an Array, or Date, or ...).
+ */
+var isObject = sg.isObject = function(x) {
+  if (!_.isObject(x))                     { return false; }
+  if (_.isArray(x)    || _.isDate(x))     { return false; }
+  if (_.isFunction(x) || _.isRegExp(x))   { return false; }
+  if (_.isError(x))                       { return false; }
+
+  return true;
+};
+
+/**
+ *
+ */
+var isPod = sg.isPod = function(x) {
+  if (_.isString(x))            { return true; }
+  if (_.isNumber(x))            { return true; }
+  if (_.isBoolean(x))           { return true; }
+  if (_.isDate(x))              { return true; }
+  if (_.isRegExp(x))            { return true; }
+
+  return false;
+};
+
+/**
+ *  Returns true if the argument === null or === undefined.
+ *
+ */
+isnt = sg.isnt = function(x) {
+  return _.isNull(x) || _.isUndefined(x);
+};
+
+/**
+ *  Returns true if any of the items in `argv` isnt().
+ *
+ */
+anyIsnt = sg.anyIsnt = function(argv) {
+  return sg.reduce(argv, false, (m, arg) => {
+    if (m !== false) { return m; }
+    return sg.isnt(arg);
+  });
+};
+
+/**
+ *  Is the value in the list-as-a-sting.
+ *
+ *  strList : 'a,foo,barbaz'
+ *  value   : 'a'
+ *
+ *  Must do ',a,foo,barbaz,'.indexOf(...)
+ */
+sg.inList = function(strList, value, sep_) {
+  var sep = sep_ || ',';
+
+  var surrounded = sep + strList + sep;
+  return surrounded.indexOf(sep + value + sep) !== -1;
+};
+
+/**
+ *  Makes the key a valid identifier (letter, digit, or underscore).
+ */
+sg.cleanKey = function(key) {
+  return key.replace(/[^a-zA-Z0-9_]/g, '_');
+};
+
+/**
+ *  Makes x the right type.
+ */
+var smartValue = sg.smartValue = function(value) {
+  if (_.isString(value)) {
+    if (value === 'true')       { return true; }
+    if (value === 'false')      { return false; }
+    if (value === 'null')       { return null; }
+
+    if (/^[0-9]+$/.exec(value)) { return parseInt(value, 10); }
+
+    // 2018-12-31T10:08:56.016Z
+    if (value.length >= 24 && value[10] == 'T') {
+      if (value.match(/\d\d\d\d-\d\d-\d\dT\d\d:\d\d:\d\d.\d\d\d/)) {
+        return new Date(value);
+      }
+    }
+
+    if (/^[0-9]+[.]([0-9]+)?$/.exec(value))   { return +value; }
+    if (/^[.][0-9]+$/.exec(value))            { return +value; }
+  }
+
+  return value;
+};
+
+/**
+ * Makes each attribute on obj the right type.
+ */
+var smartAttrs = sg.smartAttrs = function(obj) {
+  return _.reduce(obj, function(m, value, key) {
+    return sg.kv(m, key, smartValue(value));
+  }, {});
+};
+
+/**
+ *  Build {k:v}, but do not set the value if k or v are undefined/null.
+ *
+ *  This allows passing in undefined, and getting the original object
+ *  back, without mods.
+ */
+var kvSmart = sg.kvSmart = function(o, k, v) {
+  if (arguments.length === 2) {
+    return kvSmart(null, o, k);
+  }
+
+  o = o || {};
+
+  if (!isnt(k) && !isnt(v)) {
+    o[cleanKey(k)] = smartValue(v);
+  }
+
+  return o;
+};
+
+/**
+ *  Gets a sub-sub-key.
+ */
+var deref = sg.deref = function(x, keys_) {
+  if (isnt(x))      { return /* undefined */; }
+  if (isnt(keys_))  { return /* undefined */; }
+
+  var keys    = _.isArray(keys_) ? keys_.slice() : keys_.split('.'), key;
+  var result  = x;
+
+  while (keys.length > 0) {
+    key = keys.shift();
+    if (!(result = result[key])) {
+      // We got a falsy result.  If this was the last item, return it (so, for example
+      // we would return a 0 (zero) if looked up.
+      if (keys.length === 0) { return result; }
+
+      /* otherwise -- return undefined */
+      return /* undefined */;
+    }
+  }
+
+  return result;
+};
+
+/**
+ *  Sets value on object (this is the workhorse for , setOna.)
+ *
+ *  Returns the sanitized keys, or false.
+ */
+var _setOnIt = function(x, keys_, value) {
+  if (isnt(x) || isnt(keys_) || isnt(value))  { return false; }
+
+  var keys  = _.isArray(keys_) ? keys_ : keys_.split('.').map(function(x) {return x==='' ? null: x});
+
+  if (anyIsnt(keys))                          { return false; }
+
+  return keys;
+};
+
+/**
+ *  Sets sub-sub-key of object, and always returns the passed-in value.
+ *
+ *  setOn(x, 'foo.bar.baz', 42)
+ *
+ *  x = {foo:{bar:{baz:42}}}
+ *
+ *  Does not set the sub-object if value is undefined. This allows:
+ *
+ *      // if abc is not set on  options, x.foo.bar.baz does not get set
+ *      setOn(x, 'foo.bar.baz', options.abc);
+ */
+var setOn = sg.setOn = function(x, keys_, value) {
+  var keys = _setOnIt(x, keys_, value);
+  if (keys === false)                       { return value; }
+
+  var owner = x, key;
+
+  while (keys.length > 1) {
+    key = keys.shift();
+    owner[key] = owner[key] || {};
+    owner = owner[key];
+  }
+
+  if (!isnt(key = keys.shift())) {
+    owner[key] = value;
+  }
+
+  return value;
+};
+
+
+/**
+ *  Sets sub-sub-key of object as an array, and always returns the passed-in value.
+ *
+ *  setOna(x, 'foo.bar.baz', 42)
+ *
+ *  x = {foo:{bar:{baz:[42]}}}
+ *
+ *  Does not set the sub-object if value is undefined. This allows:
+ *
+ *      // if abc is not set on  options, x.foo.bar.baz does not get set
+ *      setOn(x, 'foo.bar.baz', options.abc);
+ */
+var setOna = sg.setOna = function(x, keys_, value) {
+  var keys = _setOnIt(x, keys_, value);
+  if (keys === false)                       { return value; }
+
+  var owner = x, key;
+
+  while (keys.length > 1) {
+    key           = keys.shift();
+    if (isnt(owner[key])) {
+      owner[key]  = _.isNumber(keys[0]) ? [] : {};
+    }
+    owner         = owner[key];
+  }
+
+  if (!isnt(key = keys.shift())) {
+    owner[key] = owner[key] || [];
+    owner[key].push(value);
+  }
+
+  return value;
+};
+
+/**
+ * Converts to boolean.
+ *
+ * @param {*} value
+ * @returns
+ */
+sg.trueOrFalse = function(value_) {
+  var value = value_;
+  if (value === true || value === false)  { return value; }
+  if (value === 'true')                   { return true; }
+  if (value === 'false')                  { return false; }
+
+  if (_.isString(value)) { value = +value; }    // Convert to number
+  return !!value;
+};
+
+/**
+ *  Just like _.each, except adds three params to the callback:
+ *
+ *  * The numeric index (call invocation number)
+ *  * first
+ *  * last
+ */
+sg.each = sg._each = function(collection, fn, context) {
+  var numericIndex = 0;
+  var length = collection.length || sg.numKeys(collection);
+
+  _.each(collection, function(element, index, coll) {
+    var args = [element, index, {collection: coll, i:numericIndex, first:(numericIndex === 0), last:(numericIndex+1 === length), length:length}];
+    numericIndex += 1;
+    return fn.apply(this, args);
+  }, context);
+};
+
+/**
+ * Like _.reduce, but the parameters are in the right places.
+ *
+ * @param {*} collection
+ * @param {*} initial
+ * @param {*} fn
+ * @returns
+ */
+sg.reduce = function(collection, initial, fn) {
+  return _.reduce(collection, fn, initial);
+};
+
+/**
+ *  Just like _.extend, but does not mutate the 1st arg.
+ */
+sg.extend = function() {
+  var args = sg.reduce(arguments, [], function(m, arg) {
+    return sg.ap(m, arg);
+  });
+
+  args.unshift({});
+  return _.extend.apply(_, args);
+};
+
+/**
+ *  Smart sg.extend().
+ */
+sg.smartExtend = function() {
+  var args = sg.reduce(arguments, [], function(m, arg) {
+    return sg.ap(m, sg.isObject(arg) ? sg.smartAttrs(arg) : arg);
+  });
+
+  args.unshift({});
+  return _.extend.apply(_, args);
+};
+
+/**
+ * Pulls the item out of the object and returns it.
+ *
+ * @param {*} collection_
+ * @param {*} name
+ * @returns
+ */
+sg.extract = function(collection_, name) {
+  var collection  = collection_ || {};
+  var value       = collection[name];
+
+  delete collection[name];
+  return value;
+};
+
+/**
+ * Pulls the items out of the object and returns a new object with those items.
+ *
+ * @param {*} collection
+ * @returns
+ */
+sg.extracts = function(collection /*, names... */) {
+  var names  = _.rest(arguments);
+  var result = {};
+
+  _.each(names, function(name) {
+    result[name] = sg.extract(collection, name);
+  });
+
+  return result;
+};
+
+/**
+ *  Make sure the item is an array.
+ */
+sg.toArray = function(x) {
+  if (x === null || _.isUndefined(x)) { return []; }
+  if (_.isArray(x))                   { return x; }
+  return [x];
+};
+
+var safeJSONParse = sg.safeJSONParse = function(str) {
+  if (str !== '') {
+    try {
+      return JSON.parse(str);
+    } catch(err) {
+//      console.error("Error parsing JSON", str, err);
+    }
+  }
 };
 
 
