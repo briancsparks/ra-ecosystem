cb320fe27e6f22aa4d2b022d0637d842450331fc
Brian C Sparks <brian.c.sparks@hp.com>
2019-01-03 00:09:06 -0800

    wip

--- packages/aws-command/lib/ec2/vpc.js
+++ packages/aws-command/lib/ec2/vpc.js
@@ -3,6 +3,7 @@
  * @file
  */
 
+const _                       = require('lodash');
 const sg                      = require('sg-flow');
 const ra                      = require('run-anywhere').v2;
 const AWS                     = require('aws-sdk');
@@ -17,30 +18,68 @@ mod.xport({getSubnets: function(argv, context, callback) {
 
   const classB = argv.classB;
 
+  var   allVpcs, allSubnets, allSecurityGroups;
+
   return sg.__runll([function(next) {
-    return next();
-  }, function(next) {
-    return next();
-  }, function(next) {
-    return next();
-  }], function done() {
+
     return ec2.describeVpcs({}, function(err, data) {
-      // console.log(sg.inspect(data), err);
+      if (!sg.ok(err, data)) { return callback(err); }
 
-      var result = [];
-
-      if (classB) {
-        result = sg.reduce(data.Vpcs || [], result, function(m, vpc) {
-          let parts = (vpc.CidrBlock || '').split(/[^0-9]+/);
-          if (parts.length === 5 && parts[1] === classB) {
-            return sg.ap(m, vpc);
-          }
-          return m;
-        });
-      }
-
-      return callback(err, result);
+      allVpcs = data.Vpcs;
+      return next();
     });
+  }, function(next) {
+
+    return ec2.describeSecurityGroups({}, function(err, data) {
+      if (!sg.ok(err, data)) { return callback(err); }
+
+      allSecurityGroups = data.SecurityGroups;
+      return next();
     });
+  }, function(next) {
+
+    return ec2.describeSubnets({}, function(err, data) {
+      if (!sg.ok(err, data)) { return callback(err); }
+
+      allSubnets = data.Subnets;
+      return next();
+    });
+  }], function done() {
+
+    var result = {};
+    var vpcs = [], subnets = [], securityGroups = [];
+
+    if (classB) {
+      vpcs = sg.reduce(allVpcs || [], vpcs, function(m, vpc) {
+        let parts = (vpc.CidrBlock || '').split(/[^0-9]+/);
+        if (parts.length === 5 && parts[1] === classB) {
+          return sg.ap(m, vpc);
+        }
+        return m;
+      });
+    }
+
+    _.each(vpcs, function(vpc) {
+      subnets = sg.reduce(allSubnets, subnets, function(m, subnet) {
+        if (subnet.VpcId === vpc.VpcId) {
+          return sg.ap(m, subnet);
+        }
+        return m;
+      });
+
+      securityGroups = sg.reduce(allSecurityGroups, securityGroups, function(m, securityGroup) {
+        if (securityGroup.VpcId === vpc.VpcId) {
+          return sg.ap(m, securityGroup);
+        }
+        return m;
+      });
+
+    });
+
+    result.vpcs = vpcs;
+    result.subnets = subnets;
+    result.securityGroups = securityGroups;
+    return callback(null, result);
+  });
 
 }});
