09b58e453141718ce98021aebb6c51d23bce25de
Brian C Sparks <brian.c.sparks@hp.com>
2019-02-04 19:13:34 -0800

    a bit more sophistication with argv

--- packages/sg-argv/__tests__/sg-argv.test.js
+++ packages/sg-argv/__tests__/sg-argv.test.js
@@ -10,4 +10,29 @@ test('ARGV simple', t => {
   t.snapshot(ARGV);
 });
 
+test('ARGV simple short', t => {
+  const argv      = 'a b -l'.split(' ');
+
+  const ARGV = sg.ARGV(argv);
+
+  t.snapshot(ARGV);
+});
+
+test('ARGV simple array', t => {
+  const argv      = 'a b --arr= 1 2'.split(' ');
+
+  const ARGV = sg.ARGV(argv);
+
+  // t.deepEqual(ARGV, {_:[], arr:[1,2]});
+  t.snapshot(ARGV);
+});
+
+test('ARGV array with snake key', t => {
+  const argv      = 'a b --arr-two= 1 2'.split(' ');
+
+  const ARGV = sg.ARGV(argv);
+
+  // t.deepEqual(ARGV, {_:[], arr_two:[1,2]});
+  t.snapshot(ARGV);
+});
 
--- packages/sg-argv/index.js
+++ packages/sg-argv/index.js
@@ -4,6 +4,7 @@
  */
 
 const sg                      = require('sg0');
+const { _ }                   = sg;
 
 sg.ARGV = ARGV;
 // console.log(sg.inspect(ARGV));
@@ -17,17 +18,60 @@ function snake_case(key) {
 }
 
 function ARGV(input = process.argv) {
-  var argv = require('minimist')(input.slice(2));
+  var args = [], argv = {};
 
-  argv = sg.reduce(sg.keys(argv), argv, function(m, key) {
-    const snaked = snake_case(key);
-    if (key === snaked) {
-      return m;
+  args    = preProcess(input.slice(2), argv);
+  argv    = sg.smartExtend(argv, require('minimist')(args));
+  // var argv2   = {};
+
+  const keys = sg.keys(argv);
+  for (var i = 0; i < keys.length; ++i) {
+    let   key     = keys[i];
+    let   snaked  = snake_case(key);
+
+    // i = arrayParam(i, argv, keys);
+
+    if (key !== snaked) {
+      argv[snaked] = argv[keys[i]];
     }
-
-    return sg.kv(m, snaked, argv[key]);
-  });
+  }
 
   return argv;
+}
+
+function preProcess(args, argv) {
+  var   result = [];
+
+  var old;
+  for (var i = 0; i < args.length;) {
+    old = i;
+    i   = arrayParam(i, result, args, argv);
+
+    if (i === old) {
+      result.push(args[i]);
+      i += 1;
+    }
+  }
+
+  return result;
+}
+
+function arrayParam(i, _, args, argv) {
+  // const key0 = keys[i];
+
+  // if (typeof argv[key0] !== 'string')           { return i; }
+
+  var   m;
+
+  if ((m = args[i].match(/--([^=]+)=$/))) {
+    let key = snake_case(m[1]);
+    argv[key] = [];
+
+    for (++i; i < args.length; ++i) {
+      argv[key].push(sg.smartValue(args[i]));
+    }
+  }
+
+  return i;
 }
 
