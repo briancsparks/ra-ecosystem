9e7decbebdda2a0f1fa2e3959857f233237ec60e
Brian C Sparks <brian.c.sparks@hp.com>
2019-09-21 17:15:56 -0700

    refactoring

--- packages/run-anywhere/platform/platform-utils.js
+++ packages/run-anywhere/platform/platform-utils.js
@@ -1,10 +1,14 @@
 
+const sg                        = require('sg0');
+const _                         = require('lodash');
+
 const useSmEvents   = !!process.env.SG_LOG_SMALL_EVENTS || true;      // TODO: remove
 
 module.exports.normalizeEvent = normalizeEvent;
 
 function normalizeEvent(event_) {
-  const body    = event_.body || {payload:[]};
+  const body      = decodeBody(event_);
+  // const body    = event_.body || {payload:[]};
 
   var   event = {...event_};
 
@@ -14,3 +18,32 @@ function normalizeEvent(event_) {
 
   return event;
 }
+
+function decodeBody(event) {
+  const {body, isBase64Encoded} = event;
+
+  if (sg.isnt(body))        { return body; }
+  if (!_.isString(body))    { return body; }    /* already parsed */
+
+  var body_ = body;
+
+  if (isBase64Encoded) {
+    const buf   = new Buffer(body, 'base64');
+    body_       = buf.toString('ascii');
+  }
+
+  body_ = sg.safeJSONParse(body_)   || {payload:[]};
+
+  // Make much smaller sometimes
+  if (sg.modes().debug) {
+    if (Array.isArray(body_.payload) && body_.payload.length > 1) {
+      body_ = {...body_, payload: [body_.payload[0], `${body_.payload.length} more items.`]};
+    }
+  }
+
+  event.body              = body_;
+  event.isBase64Encoded   = false;
+
+  return body_;
+}
+
