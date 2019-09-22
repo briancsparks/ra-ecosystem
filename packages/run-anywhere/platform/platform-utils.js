d23a1cd5534fcb82017a2eba69410fe3698db06b
Brian C Sparks <brian.c.sparks@hp.com>
2019-09-21 17:51:24 -0700

    refactoring

--- packages/run-anywhere/platform/platform-utils.js
+++ packages/run-anywhere/platform/platform-utils.js
@@ -8,12 +8,14 @@ module.exports.normalizeEvent = normalizeEvent;
 
 function normalizeEvent(event_) {
   const body      = decodeBody(event_);
-  // const body    = event_.body || {payload:[]};
 
   var   event = {...event_};
 
-  if (useSmEvents) {
-    event   = {...event, body: {...body, payload: [body.payload[0] ||{}, `...and ${body.payload.length} more.`]}};
+  // Must have body for these
+  if (body) {
+    if (useSmEvents) {
+      event   = {...event, body: {...body, payload: [body.payload[0] ||{}, `...and ${body.payload.length} more.`]}};
+    }
   }
 
   return event;
