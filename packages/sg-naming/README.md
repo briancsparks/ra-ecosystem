8a454985a7cf7e8eee8563b9160deb88f5e19c20
Brian C Sparks <brian.c.sparks@hp.com>
2019-11-29 17:44:01 -0800

    Some work on sg-naming

--- packages/sg-naming/README.md
+++ packages/sg-naming/README.md
@@ -4,10 +4,31 @@ The naming convention for SG.
 
 ## Simple
 
-Package; [sub-package...], project, version, variation
+```
+[upstream...] / Package: [sub-packages...] / project: [sub-projects...] / version / variation / [item-names...] (= item-value)
 
-* Package is known.
-* A place for sub-packages, but hopefully not needeed
+ - or -
+
+[upstream...] / Project: [sub-projects...] / package: [sub-packages...] / version / variation / [item-names...] (= item-value)
+```
+
+* Upstream is not our concern, but it exists.
+* Since the package is the one using the namespacing, the `package` and `sub-packages` are known.
+* A place for sub-packages
+  * But in reality, this will usually get folded into the parent.
 * The specific project
-* The project's version number
-* The project's variation (aka 'stage' or 'build')
+* A place for sub-projects
+  * But in reality, this will usually get folded into the parent.
+* The item's version number
+  * Semantic numbering, but each digit has a default ([0.0.1]), but can be written many ways, like v1_2
+* The item's variation (aka 'stage' or 'build')
+  * Most if not all items share the path, but they don't have to (default to `demo`)
+* After our segment, there are possibly infinite more path segments, but we are not in control of
+  them.
+
+## Well Known Formats
+
+* Redis uses colons.
+* Windows uses back-slashes
+* The rest of the world uses forward slashes.
+* Etc.
--- packages/sg-naming/index.js
+++ packages/sg-naming/index.js
@@ -1,24 +1,79 @@
 
 module.exports.mk_sg_name = mk_sg_name;
 
+var styles = {redis, path, url, winpath};
 
-function mk_sg_name(packaje_, ...restOfPackages) {
+function mk_sg_name(packajeJoin, packaje_, restOfPackagesArray, projectJoin, project_, restOfProjectArray, projectIsFirst) {
 
   // package should be supplied, but figure out from env if not
-  const packaje = packaje_ || process.env[`SG_NAMING_PACKAGE_NAME`];
+  const packaje__ = packaje_ || process.env[`SG_NAMING_PACKAGE_NAME`];
 
-  return function(project, version, variation) {
+  const packaje   = (packajeJoin || defJoiner)(packaje_, ...restOfPackagesArray);
+  const project   = (projectJoin || defJoiner)(project_, ...restOfProjectArray);
 
-    // TODO: Should be able to figure these out from env
+  const [A, B]    = projectIsFirst ? [project, packaje] : [packaje, project];
 
-    return function(style) {
-      const sep = style;
-
-      function sg_name(key, ...restOfKey) {
-        return [packaje, ...restOfPackages, project, version, variation, key, ...restOfKey].reduce((m,v) => m+ (is(v) ? v+sep : ''), '');
-      }
-    };
+  const fns = {
+    area:   function(version, variation) { return function(style) { return mkPenUltimate({version, variation, style}); }; },
+    style:  function(style) { return function(version, variation) { return mkPenUltimate({version, variation, style}); }; },
   };
+
+  return fns;
+
+  // return function(project, version, variation) {
+
+  //   // TODO: Should be able to figure these out from env
+
+  //   return function(style) {
+  //     const sep = style;
+
+  //     function sg_name(key, ...restOfKey) {
+  //       return [packaje, ...restOfPackagesArray, project, version, variation, key, ...restOfKey].reduce((m,v) => m+ (is(v) ? v+sep : ''), '');
+  //     }
+  //   };
+  // };
+
+  function mkPenUltimate({version, variation, style}) {
+    // const prefix  = _compact([A || 'prj', B, version, variation]);
+    const prefix  = [A || 'prj', B, version, variation];
+    const styleFn = styles[style] || styles.path;
+
+    return function access(...compoundKey) {
+      return styleFn(prefix, compoundKey);
+    };
+  }
+}
+
+function defJoiner(...args) {
+  _compact(args).join(':');
+}
+
+function redis(prefix_, compoundKey) {
+  const prefix  = _compact(prefix_);
+  const str     = [prefix, ...compoundKey].join(':');
+
+  return str;
+}
+
+function path() {
+}
+
+function url() {
+}
+
+function winpath() {
+}
+
+function _compact([args]) {
+  return args.reduce((acc, arg) => (isnt(arg) ? acc : [...acc, arg], []));
+}
+
+function isnt(x) {
+  if (x === undefined)  { return true; }
+  if (x === null)       { return true; }
+  if (x !== x)          { return true; }    /* NaN */
+
+  return true;
 }
 
 function lookupStyle(style) {
